c474cb50658db35ca45e0bff22a870d0dc1c703f
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2020-07-05 22:19:41 -0700

    Fix pylint warning and rename classes appropriately
    Changes  to  fix  many pylint  warnings  and fixing
    many naming for classes to make it more meaningful
    
    Signed-off-by: Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>

--- daos_runner/__init__.py
+++ daos_runner/__init__.py
@@ -1,3 +1,4 @@
-from DAOSEnv	import DAOSEnv
-from DAOSPool	import DAOSPool
-from DAOSRunner import DAOSRunner
+""" __init__.py script for the  daos_runner project"""
+from doos_env	import DaosBashEnv
+from daos_pool	import DaosPool
+from daos_runner import DaosRunner
